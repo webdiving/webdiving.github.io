c433ac123ad7acedaeed76a35b8ce0c6dcece40c
Loïc Fuentes <fuentes.loic@gmail.com>
2015-01-12 16:47:54 +0100

    on Grunt : sass module correction and adding watch module

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,16 +9,42 @@ module.exports = function(grunt) {
 
     // Les tâches sont enregistrées ici
     grunt.initConfig({
+        sass: {                              // Task
+            dist: {                            // Target
+                options: {
+                    sourcemap : true
+                },
+                files: {                         // Dictionary of files
+                    'css/main.css': 'css/main.scss'      // 'destination': 'source'
+                }
+            }
+        },
+    
+
         autoprefixer: {
             options: {
-                    // Task-specific options go here.
+            // Task-specific options go here.
             },
             no_dest: {
                 src: 'css/main.css' // globbing is also possible here
             },
         },
+        
+        watch: {
+            dist: {
+                files: ['css/main.scss'],
+                task: ['default'],
+                options: {
+                    spawn: false
+                }
+            }
+        }
     })
     
+    grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
+    grunt.registerTask('default', ['sass', 'autoprefixer']);
+    
 };
